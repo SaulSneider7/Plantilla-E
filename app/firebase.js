c170d16acef62720887502519176fbff8d588cbc
DevSaulSneider <saulsneiderwork@outlook.com>
2023-03-07 16:49:12 -0500

    Credenciales de Firebase

--- app/firebase.js
+++ app/firebase.js
@@ -3,16 +3,19 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/9.17.1/firebas
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 import { getAuth } from "https://www.gstatic.com/firebasejs/9.17.1/firebase-auth.js";
-//import { getFirestore } from "https://www.gstatic.com/firebasejs/9.10.0/firebase-firestore.js"
+//import { getFirestore } from "https://www.gstatic.com/firebasejs/9.10.1/firebase-firestore.js"
 
-// Your web app's Firebase configuration
+//====================================================
+//REEMPLAZAR POR TUS PROPIAR CREDENCIALES DE FIREBASE
+//====================================================
 const firebaseConfig = {
-    apiKey: "AIzaSyBsoVY1OuziKwMj2rwOYz3xRZC1QeL3IQk",
-    authDomain: "fir-app-tuto-5c20f.firebaseapp.com",
-    projectId: "fir-app-tuto-5c20f",
-    storageBucket: "fir-app-tuto-5c20f.appspot.com",
-    messagingSenderId: "464717605656",
-    appId: "1:464717605656:web:03e31531db8fed6379cc6d",
+    apiKey: "AIzaSyDoqtjthHH4p41v-T_OSwZfjuWFDiqMx4A",
+    authDomain: "fir-demo-47ca8.firebaseapp.com",
+    projectId: "fir-demo-47ca8",
+    storageBucket: "fir-demo-47ca8.appspot.com",
+    messagingSenderId: "406464430604",
+    appId: "1:406464430604:web:09e8800712d6dac9d26429",
+    measurementId: "G-GRP5HLGXKY",
 };
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig); //recordar export es para poder importar app en otros archivos
