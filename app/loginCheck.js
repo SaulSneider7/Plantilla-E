4b7c1bb6dcc87ec44466e8c703c82ffe36b5a076
saulsneider7 <saulsneider@hotmail.com>
2023-03-13 08:50:53 -0500

    loginchexk ok

--- app/loginCheck.js
+++ app/loginCheck.js
@@ -1,16 +1,19 @@
 const loggedOutLinks = document.querySelectorAll(".logged-out");
 const loggedInLinks = document.querySelectorAll(".logged-in");
 
+// crear variable
+const loggedInputs = document.querySelectorAll(".card");
+
 export const loginCheck = (user) => {
     if (user) {
         loggedInLinks.forEach((link) => (link.style.display = "block"));
         loggedOutLinks.forEach((link) => (link.style.display = "none"));
         // Agregar
-        loggedInputs.forEach((div) => (div.style.display = "none"));
+        loggedInputs.forEach((div) => (div.style.display = "block"));
     } else {
         loggedInLinks.forEach((link) => (link.style.display = "none"));
         loggedOutLinks.forEach((link) => (link.style.display = "block"));
          // Agregar
-         loggedInputs.forEach((div) => (div.style.display = "block"));
+         loggedInputs.forEach((div) => (div.style.display = "none"));
     }
 };
